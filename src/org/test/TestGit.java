a77ca74f7f983c23f5298e42528322d47c07b6f3
anandgitacc <anand.akmail@gmail.com>
2022-02-01 16:08:36 +0530

    firstgit

---
+++ src/org/test/TestGit.java
@@ -0,0 +1,28 @@
+package org.test;
+
+public class TestGit {
+	
+private void empName() {
+		
+		System.out.println("Employee Name is : Anand");
+	}
+	private void empMobNo() {
+		
+		System.out.println("Employee mobile no is : 8667070832");
+	}
+	
+	private void empMob() {
+		
+		System.out.println("Employee mobile : 444");
+	}
+	
+	public static void main(String[] args) {
+		
+		TestGit ed = new TestGit();
+		
+		ed.empName();
+		ed.empMobNo();
+		
+	}
+
+}
